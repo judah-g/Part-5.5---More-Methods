817a4ad8333d73a6329088090df3e22c63c961c4
Judah Godden <339633265@GoTVDSB.ca>
2024-09-17 09:15:56 -0400

    done

---
+++ Part 5.5 - More Methods/Die.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5._5___More_Methods
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die() 
+        { 
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        public int Roll
+        { get { return _roll; } }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+        }
+
+        public void DrawDie()
+        {
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----\r\n|   |\r\n| o |\r\n|   |\r\n-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----\r\n|o  |\r\n|   |\r\n|  o|\r\n-----");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----\r\n|o  |\r\n| o |\r\n|  o|\r\n-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----\r\n|o o|\r\n|   |\r\n|o o|\r\n-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----\r\n|o o|\r\n| o |\r\n|o o|\r\n-----");
+            }
+            else
+            {
+                Console.WriteLine("-----\r\n|o o|\r\n|o o|\r\n|o o|\r\n-----");
+            }
+        }
+    }
+}
--- Part 5.5 - More Methods/Program.cs
+++ Part 5.5 - More Methods/Program.cs
@@ -10,6 +10,31 @@ namespace Part_5._5___More_Methods
     {
         static void Main(string[] args)
         {
+            //big j
+
+            Die die1 = new Die(), die2 = new Die();
+
+            die1.RollDie(); die2.RollDie(); die2.RollDie();
+            die1.DrawDie(); die2.DrawDie();
+
+            if (die1.Roll + die2.Roll == 7)
+            { Console.WriteLine("Your dice have a sum of 7"); }
+            if (die1.Roll + die2.Roll == 2)
+            { Console.WriteLine("You rolled snake eyes"); }
+            if ((die1.Roll + die2.Roll) % 2 == 0)
+            { Console.WriteLine("Your numbers have even sum"); }
+            if (die1.Roll + 1 == die2.Roll || die2.Roll + 1 == die1.Roll)
+            { Console.WriteLine("You have sequential dice"); }
+
+            if (die1.Roll == die2.Roll)
+            { Console.WriteLine("You rolled doubles"); }
+            else if (die1.Roll > die2.Roll)
+            { Console.WriteLine("Die 1 is bigger than die 2"); }
+            else { Console.WriteLine("Die 2 is bigger than die 1"); }
+
+            Console.WriteLine();
+            Console.Write("Press enter to exit:\t");
+            Console.Read();
         }
     }
 }
